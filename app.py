3987f128d0dd7bcd7cfa1e625db9c487df3c1434
Leander Rodrigues <me@leander.xyz>
2021-08-10 11:22:14 -0400

    :boom: make some mistakes

---
+++ app.py
@@ -0,0 +1,21 @@
+import sentry_sdk
+from flask import Flask
+from sentry_sdk.integrations.flask import FlaskIntegration
+
+sentry_sdk.init(
+    dsn="https://f61444722ce0460892f94a6d5d110596@o951660.ingest.sentry.io/5900755",
+    integrations=[FlaskIntegration()],
+    traces_sample_rate=1.0
+)
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def home():
+    return "<p> Working hello world!</p"
+
+
+@app.route("/error")
+def error():
+    raise RuntimeError("something is breaking 😬")
