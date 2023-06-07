9e16faef9f726dec737c0be979b37ac7f4408fe1
Leander Rodrigues <leander.rodrigues@sentry.io>
2023-06-07 12:05:28 -0700

    🛠️ Config changes

--- flask-error/app.py
+++ flask-error/app.py
@@ -4,7 +4,7 @@ from sentry_sdk.integrations.flask import FlaskIntegration
 import argparse
 
 LOCAL_SENTRY_DSN = "https://93b7b137357b4edd9fbb538ee398b7e9@leeandher.ngrok.io/2"
-LOCAL_GETSENTRY_DSN = "http://bd63f9383e4747a89fa5015a94616197@dev.getsentry.net:8000/2"
+LOCAL_GETSENTRY_DSN = "https://ee8749033b194d888fe17531eac25a35@leeandher.ngrok.io/4505319664189440"
 HOSTED_DSN = "https://f61444722ce0460892f94a6d5d110596@o951660.ingest.sentry.io/5900755"
 MARCOS_DSN = "https://9db93bc8a4f011eaa82d4201c0a8d032@o401775.ingest.sentry.io/5261902"
 
@@ -33,7 +33,8 @@ def dsn_selector():
 sentry_sdk.init(
     dsn=dsn_selector(),
     integrations=[FlaskIntegration()],
-    traces_sample_rate=1.0
+    traces_sample_rate=1.0,
+    release='abc'
 )
 
 app = Flask(__name__)
