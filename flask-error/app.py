4ec7566ea93492c4fbb052d1d4a30f5e1288efe1
Leander Rodrigues <leander.rodrigues@sentry.io>
2023-09-29 16:28:52 -0400

    ✨ ado test changes

--- flask-error/app.py
+++ flask-error/app.py
@@ -3,29 +3,30 @@ from flask import Flask
 from sentry_sdk.integrations.flask import FlaskIntegration
 import argparse
 
-LOCAL_SENTRY_DSN = "https://93b7b137357b4edd9fbb538ee398b7e9@leeandher.ngrok.io/2"
-LOCAL_GETSENTRY_DSN = "https://ee8749033b194d888fe17531eac25a35@leeandher.ngrok.io/4505319664189440"
+LOCAL_SENTRY_DSN = "https://96e4bfeea741615f0d83ca82c187ec5d@leeandher.ngrok.io/18"
+LOCAL_GETSENTRY_DSN = (
+    "https://ee8749033b194d888fe17531eac25a35@leeandher.ngrok.io/4505319664189440"
+)
 HOSTED_DSN = "https://6338209daaba4a868fca858e3f7ebfc2@o951660.ingest.sentry.io/6507927"
-MARCOS_DSN = "https://9db93bc8a4f011eaa82d4201c0a8d032@o401775.ingest.sentry.io/5261902"
 
 parser = argparse.ArgumentParser(description="Create some sentry errors")
-parser.add_argument('instance',
-                    default='local sentry',
-                    const='sentry',
-                    nargs='?',
-                    choices=['local sentry', 'local getsentry', 'hosted'],
-                    help='Sentry instance to receive errors')
+parser.add_argument(
+    "instance",
+    default="local sentry",
+    const="sentry",
+    nargs="?",
+    choices=["local sentry", "local getsentry", "hosted"],
+    help="Sentry instance to receive errors",
+)
 
 
 def dsn_selector():
     args = parser.parse_args()
     print(f"Sending errors to '{args.instance}' instance...")
-    if args.instance == 'local getsentry':
+    if args.instance == "local getsentry":
         return LOCAL_GETSENTRY_DSN
-    elif args.instance == 'hosted':
+    elif args.instance == "hosted":
         return HOSTED_DSN
-    elif args.instance == 'marcos':
-        return MARCOS_DSN
     else:
         return LOCAL_SENTRY_DSN
 
@@ -46,7 +47,8 @@ def home():
 
 @app.route("/error")
 def error():
-    sentry_sdk.capture_exception(ValueError("TestError5"))
+    print(1 / 0)
+    # sentry_sdk.capture_exception(ValueError("TestError5"))
 
 
 if __name__ == "__main__":
